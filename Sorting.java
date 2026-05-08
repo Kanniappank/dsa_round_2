49a3b3494030bd5224c30211d24b974a74f54051
Suriya <suriyastart007@gmail.com>
2026-05-08 08:34:15 +0530

    Started Sorting

---
+++ Sorting.java
@@ -0,0 +1,42 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Sorting {
+
+    private static int[] selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[min] >= arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
+
+    private static int[] bubleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 7, 4, 1, 5, 3 };
+        System.out.println("sorted array is " + Arrays.toString(bubleSort(arr)));
+
+    }
+}
