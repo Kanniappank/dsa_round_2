6fe5d360fcce5cdf2dfa5ce4d83559199b9be69c
Suriya <suriyastart007@gmail.com>
2026-05-09 08:53:06 +0530

    wip on sorting

--- Sorting.java
+++ Sorting.java
@@ -21,8 +21,8 @@ public class Sorting {
 
     private static int[] bubleSort(int[] arr) {
         int n = arr.length;
-        for (int i = 0; i < n-1; i++) {
-            for (int j = 0; j < n-i-1; j++) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
@@ -33,10 +33,28 @@ public class Sorting {
         return arr;
     }
 
+    private static int[] insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n ; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                swap(arr,j,j-1);
+                j--;
+            } 
+        } 
+        return arr;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+
     public static void main(String[] args) {
 
         int[] arr = { 7, 4, 1, 5, 3 };
-        System.out.println("sorted array is " + Arrays.toString(bubleSort(arr)));
+        System.out.println("sorted array is " + Arrays.toString(insertionSort(arr)));
 
     }
 }
