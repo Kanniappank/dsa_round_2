f6bd1f6c1469b4e3adb0ecb4b788d2071f7a292e
Suriya <suriyastart007@gmail.com>
2026-05-11 08:59:23 +0530

    quick sort completed

--- Sorting.java
+++ Sorting.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Sorting {
@@ -35,13 +36,13 @@ public class Sorting {
 
     private static int[] insertionSort(int[] arr) {
         int n = arr.length;
-        for (int i = 0; i < n ; i++) {
+        for (int i = 0; i < n; i++) {
             int j = i;
             while (j > 0 && arr[j - 1] > arr[j]) {
-                swap(arr,j,j-1);
+                swap(arr, j, j - 1);
                 j--;
-            } 
-        } 
+            }
+        }
         return arr;
     }
 
@@ -51,10 +52,80 @@ public class Sorting {
         arr[i] = temp;
     }
 
+    private static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+    }
+
+    private static void mergeSort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    private static int findPartitionIndex(int[] arr, int low, int high) {
+        int piviot = arr[low];
+        int i = low;
+        int j = high;
+        while (i < j) {
+            while (arr[i] <= piviot && i < high) {
+                i++;
+            }
+            while (arr[j] > piviot && j > low) {
+                j--;
+            }
+            if (i < j) {
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, low, j);
+        return j;
+    }
+
+    private static void quickSortHelper(int[] arr, int low, int high) {
+        if (low < high) {
+            int partitionIndex = findPartitionIndex(arr, low, high);
+            System.out.println("partition index " + partitionIndex);
+            quickSortHelper(arr, low, partitionIndex - 1);
+            quickSortHelper(arr, partitionIndex + 1, high);
+        }
+    }
+
+    private static int[] quickSort(int[] nums) {
+        quickSortHelper(nums, 0, nums.length - 1);
+        return nums;
+    }
+
     public static void main(String[] args) {
 
         int[] arr = { 7, 4, 1, 5, 3 };
-        System.out.println("sorted array is " + Arrays.toString(insertionSort(arr)));
+        System.out.println("sorted array is " + Arrays.toString(quickSort(arr)));
 
     }
 }
